99d59ba3015241f8e4d8c2eeaacdc47d93ea46c9
Yulia Kuzakova <y.g.kuzakova@gmail.com>
2016-06-21 13:20:45 +0600

    Add simple link

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -2,8 +2,10 @@
 // Copyright 1999-2016. Parallels IP Holdings GmbH.
 $messages = [
     'service1Title' => 'Service title1',
+    'service4Title' => 'Service title2',
     'service2Title'=> 'Add new domain',
     'action1Title' => 'Start Service',
     'action3Title' => 'Stop Service',
-    'toolbarTitle' => 'toolbar in service'
+    'toolbarTitle' => 'toolbar in service',
+    'serviceMessage1' => 'Service Message'
 ];
